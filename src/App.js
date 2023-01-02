658c1c2de941f103cc10e1b11c327857d3b059eb
McDony Lee <mcdonyleee@gmail.com>
2023-01-02 22:21:08 +1100

    Added basic skeleton of the app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
-import logo from "./logo.svg";
-import "./App.css";
-
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./components/pages/Home";
+import About from "./components/pages/About";
+import NotFound from "./components/pages/NotFound";
+import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
 function App() {
     return (
-        <>
-            <h1 className="text-3xl font-bold underline">
-                Hello React tailwind and daisy!
-            </h1>
-            <button class="btn btn-primary">One</button>
-        </>
+        <Router>
+            <div className="flex flex-col justify-between h-screen">
+                <Navbar />
+                <main className="container mx-auto px-3 pb-12">
+                    <Routes>
+                        <Route path="/" element={<Home />}></Route>
+                        <Route path="/about" element={<About />}></Route>
+                        <Route path="/notfound" element={<NotFound />}></Route>
+                        <Route path="/*" element={<NotFound />}></Route>
+                    </Routes>
+                </main>
+                <Footer></Footer>
+            </div>
+        </Router>
     );
 }
 
---
+++ src/components/pages/About.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const About = () => {
+    return (
+        <div>
+            <h1 className="text-6xl mb-4">GitHub Finder</h1>
+        </div>
+    );
+};
+
+export default About;
